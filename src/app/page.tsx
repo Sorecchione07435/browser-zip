10887613e6f3eb0c70bb148a45a8b42e9593a6c2
Stefor07 <Sorecchione07@gmail.com>
2025-09-19 16:25:57 +0200

    main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,100 @@
-import Image from "next/image";
+"use client"
+
+import { useRef } from "react";
+import React, { useState } from 'react';
+
+type FileItem =
+  {
+    name: string;
+    size: number;
+  }
+
+const formatSize = (size: number) => {
+  // Formatta in KB / MB ecc.
+  if (size >= 1_000_000) return (size / 1_000_000).toFixed(2) + ' MB';
+  if (size >= 1_000) return (size / 1_000).toFixed(1) + ' KB';
+  return size + ' B';
+};
+
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const [items, setItems] = useState<FileItem[]>([])
+  // const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+  //   setSelectedValue(event.target.value);
+  //   console.log('Hai selezionato:', event.target.value);
+  // };
+
+  {/** const FileListView: React.FC = () => {
+  return (
+    <table style={{ width: '100%', borderCollapse: 'collapse' }}>
+      <thead>
+        <tr>
+          <th style={{ textAlign: 'left', borderBottom: '1px solid #ccc' }}>Nome file</th>
+          <th style={{ textAlign: 'right', borderBottom: '1px solid #ccc' }}>Dimensione</th>
+        </tr>
+      </thead>
+      <tbody>
+        {fileList.map((file, index) => (
+          <tr key={index}>
+            <td style={{ padding: '8px 4px' }}>{file.name}</td>
+            <td style={{ padding: '8px 4px', textAlign: 'right' }}>{formatSize(file.size)}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+ */  }
+
+  const ListView: React.FC = () => {
+    const itemList = items.map((item, index) => (
+      <tr>
+
+        <td>{item.name}</td>
+        <td> {formatSize(item.size)}</td>
+      </tr>
+    ));
+    return (
+      <div>
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <tr>
+            <th style={{ textAlign: 'left', borderBottom: '1px solid #ccc' }}>File Name</th>
+            <th style={{ textAlign: 'left', borderBottom: '1px solid #ccc' }}>File Size</th>
+          </tr>
+          {itemList}</table>
+      </div>
+    );
+  }
+
+  const fileUploader = useRef<HTMLInputElement>(null);
+
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      setItems([...items, { name: file.name, size: file.size }])
+
+
+    }
+
+  }
+  function onBrowseButtonClicked() {
+    fileUploader.current?.click();
+  }
+
+  return (
+    <div className="grid flex justify-center m-50 gap-10">
+
+      <p className="font-sans text-xl">Selezionare i files da comprimere nell'archivio:</p>
+      <p></p>
+      <ListView />
+      <input className="hidden" type="file" onChange={handleFileChange} ref={fileUploader} />
+      <button className="font-sans text-lg font-bold border-2 border-double h-8 w-full bg-yellow-200 hover:bg-red-200"
+        onClick={onBrowseButtonClicked}>Browse File...</button>
+
+
+
     </div>
   );
 }
